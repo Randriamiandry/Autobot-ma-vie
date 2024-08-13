12267ab437234b72505afa9cb96903fe479d62b7
stive0321 <165804330+stive0321@users.noreply.github.com>
2024-08-13 21:05:50 +0100

    Update ai.js

--- script/Ai.js
+++ script/Ai.js
@@ -1,29 +1,33 @@
 const axios = require('axios');
-
 module.exports.config = {
-		name: "Ai",
-		version: 1.0,
-		credits: "OtinXSandip",
-		description: "AI",
-		hasPrefix: false,
-		usages: "{pn} [prompt]",
-		aliases: ["megan","AE"],
-		cooldown: 0,
+  name: 'ai',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['gpt', 'openai'],
+  description: "An AI command powered by GPT-4",
+  usage: "Ai [promot]",
+  credits: 'Developer',
+  cooldown: 3,
 };
-
-module.exports.run = async function ({ api, event, args }) {
-		try {
-				const prompt = args.join(" ");
-				if (!prompt) {
-						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
-						return;
-				}
-
-				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-				const answer = response.data.answer;
-
-				await api.sendMessage(answer, event.threadID);
-		} catch (error) {
-				console.error("Error:", error.message);
-		}
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const input = args.join(' ');
+  if (!input) {
+    api.sendMessage(`Votre requête est comme un ordre de mon maître Stan`, event.threadID, event.messageID);
+    return;
+  }
+  api.sendMessage(` Veuillez "${input} \npatientez ....💁"`, event.threadID, event.messageID);
+  try {
+    const {
+      data
+    } = await axios.get(`https://soyeon-api.onrender.com/api?prompt=${encodeURIComponent(input)}`);
+    const response = data.response;
+    api.sendMessage(Stan_bot😉 \n response + '\n\nhttps://www.facebook.com/profile.php?id=100078426361784\n', event.threadID, event.messageID);
+  } catch (error) {
+    api.sendMessage('Erreur au niveau du recherches de ta question 🤧.', event.threadID, event.messageID);
+  }
 };
